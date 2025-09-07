6800cd18a0647452ce7df0652adbd8351fb27e04
Syl Morrison <sylmorrison@Syls-MacBook-Air.local>
2025-09-07 13:47:12 +0100

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+import {
+    mhSubscribeToParams,
+    mhUnsubscribeFromParams,
+    mhAddParameterListener,
+    mhRemoveParameterListener,
+} from "./lib/mostlyharmless_ParamEvents.js";
+
+import {
+    mhBeginParamGesture,
+    mhSetParamValue,
+    mhEndParamGesture
+} from "./lib/mostlyharmless_ParamGestures.js";
